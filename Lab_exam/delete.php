4c166f30832774ec34396eadeae66068cb762927
UBALDO17 <christianubaldo78@gmail.com>
2024-10-02 21:42:44 +0800

    add alert

--- Lab_exam/delete.php
+++ Lab_exam/delete.php
@@ -6,6 +6,7 @@
     $query = "DELETE FROM students WHERE student_id = '$id'";
 
     if (mysqli_query($conn, $query)) {
+        echo "<script>alert('Deleted Successfully!')</script>";
         echo "<script>window.location.href='main.php';</script>";
     } else {    
         echo "Failed to Delete";
